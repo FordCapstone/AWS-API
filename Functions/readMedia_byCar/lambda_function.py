2a2ac226c300b22e64c524283fb266f7fa718846
Pradoxzon <speerenboom@gmail.com>
2020-02-16 19:13:16 -0500

    Added the option to get the tag names with the media.

--- Functions/readMedia_byCar/lambda_function.py
+++ Functions/readMedia_byCar/lambda_function.py
@@ -20,12 +20,28 @@ def lambda_handler(event, context):
         reason = {"reason": msg}
         return cc.response_bad_request(json.dumps(reason))
     
+    # Get the optional tagNames
+    tagNames = cc.get_queryString(event, "tagnames")
+    
     #Create a connection to the database
     conn = cc.db_connect(os.environ["dbName"], os.environ["dbUser"],
                            os.environ["dbHost"], os.environ["dbPass"])
     
     # Return the list of media
     if(conn != None):
+        if tagNames and tagNames == "true":
+            mediaJson = get_media_by_car(conn, carId)
+            tagJson = get_tag_by_car(conn, carId)
+            
+            tagDict = {td["tagid"]:td["name"] for td in tagJson}
+            
+            for md in mediaJson:
+                md["primarytagname"] = tagDict[md["primarytag"]]
+                md["secondarytagname"] = "N/A" if md["secondarytag"] == None else tagDict[md["secondarytag"]]
+            
+            return cc.response_ok(json.dumps(mediaJson))
+        
+        # Get the media without the tag names
         return cc.response_ok(json.dumps(get_media_by_car(conn, carId)))
 
 
@@ -35,3 +51,11 @@ def get_media_by_car(conn, carId):
     Returns all the media entries as JSON
     """
     return cc.db_get_where(conn, "media", ["carid"], [carId])
+
+
+def get_tag_by_car(conn, carId):
+    """
+    Retrieves all Tags for a specific carId from the database
+    Returns all the Tags as JSON
+    """
+    return cc.db_get_where(conn, "tag", ["carid"], [carId])
